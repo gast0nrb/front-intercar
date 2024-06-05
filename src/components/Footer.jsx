69b9957b76a6eb4db419811e5229ffbde0e2d3d9
gast0nrb <developer.tongas@gmail.com>
2024-06-05 12:20:48 -0400

    Finished simple design home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,29 +4,29 @@ const Footer = ({
   mercadoLibre = "#",
 }) => {
   return (
-    <footer className="h-40 bg-neutral-800 w-full flex text-center">
-      <div className="ctc h-full w-2/6 border-r-2 my-auto border-neutral-600 pt-10">
+    <footer className="lg:h-40 bg-neutral-800 w-full md:h-auto lg:flex md:flex-row text-center lg:pb-0 md:pb-4">
+      <div className=" flex-row md:w-full ctc h-full lg:w-2/6 lg:border-r-2 my-auto lg:border-neutral-600 pt-10 md:mx-auto lg:mx-0">
         <h3 className="text-neutral-300 md:text-xs lg:text-2xl">CONTACTANOS</h3>
         <div className="ig">
-          <a className="hover:text-orange-500" href="https://www.instagram.com/intercarchile.cl">
+          <a className="text-neutral-500 hover:text-orange-500" href="https://www.instagram.com/intercarchilespa">
             <p>Instagram</p>
           </a>
         </div>
         <div className="ig">
-          <a className="hover:text-orange-500" href="https://www.instagram.com/intercarchile.cl">
-            <p>+569 42151261</p>
+          <a className="text-neutral-500 hover:text-orange-500" href="https://wa.me/+56942856029">
+            <p>+56 9 4285 6029</p>
           </a>
         </div>
         <div className="ig">
-          <a className="hover:text-orange-500" href="https://www.instagram.com/intercarchile.cl">
+          <a className="text-neutral-500 hover:text-orange-500" href="mailto:contacto@intercarchile.cl">
             <p>contacto@intercarchile.cl</p>
           </a>
         </div>
       </div>
-      <div className="lg w-2/6 border-r-2 pt-3 border-neutral-600 h-full ">
+      <div className="lg md:mx-auto lg:mx-0 w-2/6 lg:border-r-2 pt-3 lg:border-neutral-600 h-full ">
         <img className="lg:w-2/6 md:w-3/6 mx-auto" src="/logo.png" alt="" />
       </div>
-      <div className="dsg w-2/6 my-auto text-sm">
+      <div className="dsg w-2/6 my-auto text-sm md:hidden lg:block">
         <h3 className="text-neutral-500">PAGINA REALIZADA POR</h3>
         <div className="">
           <p className="text-purple-900">GRB19</p>
