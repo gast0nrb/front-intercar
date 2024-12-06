73241dbfa01398acb0ced5731453d4c9d2fdd491
gast0nrb <developer.tongas@gmail.com>
2024-12-06 15:39:37 -0300

    agregado la categoria nav

--- src/components/LayoutComponents/CategoriasNav.jsx
+++ src/components/LayoutComponents/CategoriasNav.jsx
@@ -1,34 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useContext, useState } from "react";
 import { NavContext } from "../../context/NavContext";
 import { Link } from "react-router-dom";
 
 const CategoriasNav = () => {
-  const Categorias = [
-    {
-      id: 1,
-      nombre: "HEADLIGHT",
-    },
-    {
-      id: 2,
-      nombre: "AMPOLLETAS",
-    },
-    {
-      id: 3,
-      nombre: "Seguridad",
-    },
-    {
-      id: 4,
-      nombre: "ELEctricidad",
-    },
-    {
-      id: 5,
-      nombre: "Completos",
-    },
-  ];
-
   const { changeShowUp } = useContext(NavContext);
   const [showCategorias, setShowCategorias] = useState(false);
+  const [cargandoCategorias, setCargandoCategorias] = useState(true)
+  const [categorias, setCategorias] = useState([])
+
+  async function getCategorias() {
+    const fetchCategorias = await fetch(
+      `http://localhost:8000/api/v0.5/webintercar/categorias`
+    );
+    const jsonCategorias = await fetchCategorias.json();
+    jsonCategorias.data = jsonCategorias.data.sort()
+    setCategorias([...jsonCategorias.data]);
+    setCargandoCategorias(false);
+  }
+
+  useEffect(()=> {
+  getCategorias()   
+  },[cargandoCategorias])
 
   function displayCategorias() {
     setShowCategorias(true);
@@ -67,7 +60,8 @@ const CategoriasNav = () => {
         <ul
           className={`bg-neutral-900 lg:z-50 lg:absolute p-1 w-30 text-neutral-400`}
         >
-          {Categorias.map((c, i) => (
+          { 
+          categorias.filter((v, i)=> i<=3).map((c, i) => (
             <li
               key={c.id}
               className="hover:text-orange-500 hover:border-orange-500 border-b-2 border-transparent"
--- src/pages/CategoriaPage.jsx
+++ src/pages/CategoriaPage.jsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Vacio from "../components/Vacio";
+import { useState } from "react";
 import SelectOrder from "../components/CategoriaPageComponents/SelectOrder";
 import Pagination from "../components/Pagination";
 import ProductosGroup from "../components/ProductosGroup";
@@ -9,20 +8,16 @@ import SelectCategoria from "../components/CategoriaPageComponents/SelectCategor
 const CategoriaPage = ()=> {
   const {idParams} = useParams()
   const [categoriaActual, setCategoriaActual] = useState(idParams)
-  const [cambiarCategoria, setCambiarCategoria] =useState(false)
 
   return (
     <>
      <SelectCategoria
       categoriaActual={categoriaActual}
       setCategoriaActual={setCategoriaActual}
-      cambiarCategoria={cambiarCategoria}
-      setCambiarCategoria={setCambiarCategoria}
      /> 
      <SelectOrder/>
      <ProductosGroup
       categoriaActual={categoriaActual} 
-      cambiarCategoria={cambiarCategoria}
      />
     <Pagination/>
     </>
